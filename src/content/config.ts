bc8f61d55ad93599c027336da93a341fe06edba2
Andrew <andrewtflores@yahoo.com>
2024-11-07 10:30:26 -0600

    Convert posts and projects to Content Collections

---
+++ src/content/config.ts
@@ -0,0 +1,41 @@
+import { defineCollection, z } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    authors: z.array(z.string()),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    tags: z.array(z.string()),
+    draft: z.boolean(),
+  }),
+});
+
+const projectCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    authors: z.array(z.string()),
+    contributors: z.array(z.string()),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    draft: z.boolean(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+  project: projectCollection,
+};
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,13 +1,17 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { publishedPosts } from "../utilities/getPosts.astro";
 
 export async function GET(context) {
   return rss({
     title: "AFlorzy | Blog",
     description: "Linux, Docker, Programming, and more!",
     site: context.site,
-    items: await pagesGlobToRssItems(
-      import.meta.glob("./posts/published/*.{md,mdx}")
-    ),
+    items: publishedPosts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: post.slug,
+    })),
     customData: `<language>en-us</language>`,
   });
 }
