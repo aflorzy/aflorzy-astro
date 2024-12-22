21fad3524d5bb376a25df14766c76fc910285910
Andrew Flores <andrewtflores@yahoo.com>
2024-12-21 21:34:16 -0600

    Remove Last Modified git logic

--- src/scripts/remark/remark-modified-time.mjs
+++ src/scripts/remark/remark-modified-time.mjs
@@ -1,9 +1,13 @@
-import { execSync } from "child_process";
+// import { execSync } from "child_process";
 
 export function remarkModifiedTime() {
   return function (tree, file) {
-    const filepath = file.history[0];
-    const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    // const filepath = file.history[0];
+
+    // TODO: Learn how this can execute during the compilation CI build stage
+    // const result = execSync(`git log -1 --pretty="format:%cI" "${filepath}"`);
+    const result = "";
+
     file.data.astro.frontmatter.lastModified = result.toString();
   };
 }
